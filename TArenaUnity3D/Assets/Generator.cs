5ba0d65e413fc2bce89b1dc51908186980778d91
Wojciech Lesisz <wlesisz1@gmail.com>
2019-08-05 23:39:20 +0200

    Menu

---
+++ TArenaUnity3D/Assets/Generator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Generator : MonoBehaviour
+{
+    [System.Serializable]
+    public class BuildG{
+        public int hero;
+        }
+    public void whichbuttonisselected()
+    {
+        BuildG Build = new BuildG();
+        Build.hero = 1;
+        if ("Button_bialytoster" == EventSystem.current.currentSelectedGameObject.name)
+            Build.hero = 1;
+        if ("Button_czerwonytoster" == EventSystem.current.currentSelectedGameObject.name)
+            Build.hero = 2;
+        if ("Button_niebieskitoster" == EventSystem.current.currentSelectedGameObject.name)
+            Build.hero = 3;
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/build.d";
+        System.IO.FileStream stream = new System.IO.FileStream(path, System.IO.FileMode.OpenOrCreate);
+
+        formatter.Serialize(stream, Build);
+        stream.Close();
+
+        Debug.Log(Build.hero);
+    }
+
+    
+}
---
+++ TArenaUnity3D/Assets/PanelArmii.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelArmii : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Button> buttons;
+
+
+    public class BuildG
+    {
+        public int hero;
+    }
+    void Start()
+    {
+        
+
+
+
+    }
+    public void sprawdz()
+    {
+
+        GameObject[] btn = GameObject.FindGameObjectsWithTag("herobutton");
+     
+        // Iterate through the array of 'btn' and add them to the 'buttons' list
+        for (int i = 0; i < btn.Length; i++)
+        {
+            // Adding the current 'btn' to the 'buttons' list
+            buttons.Add(btn[i].GetComponent<Button>());
+        }
+
+        string path = Application.persistentDataPath + "/build.d";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            BuildG buildG = new BuildG();
+            buildG = formatter.Deserialize(stream) as BuildG;
+            if (buildG.hero==0)
+                buttons[0].GetComponent<Image>().color = Color.white;
+            
+        }
+        else
+        {
+            Debug.Log("nie ma pliku");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
