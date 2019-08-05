2c0f9018bc47ef8971327ac0ed83e9b43487ff12
Piotr Musielski <piotrmusielski@mail.com>
2019-08-05 21:27:30 +0200

    Dev_1
    Utworzono klase Toster (postac)
    Obiekty DPS, Tank, Heal

---
+++ TArenaUnity3D/Assets/Scripts/Cielu/__Toster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class __Toster : MonoBehaviour
+{
+
+    public class Toster
+    {
+        public int BaseHealthPoints;
+        public int BaseDamage;
+        public int BaseDefence;
+
+        public Toster(int bhp, int bdmg, int bdef)
+        {
+            BaseHealthPoints = bhp;
+            BaseDamage = bdmg;
+            BaseDefence = bdef;
+        }
+
+    }
+
+    public Toster Tank_Toster = new Toster(100,2,10);
+
+    public Toster Dps_Toster = new Toster(50, 10, 2);
+
+    public Toster Heal_Toster = new Toster(50, 1, 3);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
