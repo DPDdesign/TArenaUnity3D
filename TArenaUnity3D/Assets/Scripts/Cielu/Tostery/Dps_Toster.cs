f8b13e027fcd9010e54362d0ef2f160497ad4336
Piotr Musielski <piotrmusielski@mail.com>
2019-08-05 23:08:19 +0200

    Dev 0.0.2
    Utworzono 3 Tosterów.
    Jak się na nich klika to spada im HP i się wyswietla XD
    
    Dorobić:
    Żeby statystyki tosterów wczytywały się z jakiegoś pliku
    
    Klikasz na enemy - spada hp tosterom
    
    Healer - leczy
    DPS - bije
    Tank - zwieksza defa (3 uzycia max)

---
+++ TArenaUnity3D/Assets/Scripts/Cielu/Tostery/Dps_Toster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Dps_Toster : MonoBehaviour
+{
+    public static int hp = 50;
+    public static int dmg = 5;
+    public static int def = 2;
+    string name = "Dps Toster";
+
+    void OnMouseDown()
+    {
+        hp--;
+        Debug.Log(name + " HP: " + hp);
+    }
+
+    void Update()
+    {
+        
+    }
+}
