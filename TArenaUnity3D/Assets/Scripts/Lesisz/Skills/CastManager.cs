82fc9827ed293e64023494149c0a3d45d7e6d47d
Piotr Musielski <piotrmusielski@mail.com>
2019-08-16 20:28:27 +0200

    Structure
    UI moje xD
    Strutkura frakcji

--- TArenaUnity3D/Assets/Scripts/Lesisz/Skills/CastManager.cs
+++ TArenaUnity3D/Assets/Scripts/Lesisz/Skills/CastManager.cs
@@ -8,7 +8,7 @@ public class CastManager : MonoBehaviour
 {
     public MouseControler mouseControler;
     int kochamizabelke = 0;
-    public  bool RangeSelectingenemy = false;
+    public bool RangeSelectingenemy = false;
     public bool selectingfriend = false;
     public bool unselectaround = false;
     public bool isAoE = false;
@@ -22,31 +22,31 @@ public class CastManager : MonoBehaviour
     {
         Type type = this.GetType();
         MethodInfo method = type.GetMethod(spellID);
-        method.Invoke(this,null);
-     
+        method.Invoke(this, null);
+
     }
     public void getMode(string spellID)
     {
         Type type = this.GetType();
-        MethodInfo method = type.GetMethod(spellID+"M");
+        MethodInfo method = type.GetMethod(spellID + "M");
         method.Invoke(this, null);
     }
 
     public void SetFalse()
     {
-             RangeSelectingenemy = false;
-      selectingfriend = false;
-      unselectaround = false;
-      isAoE = false;
-      aoeradius = 0;MouseControler.SkillState = false;
-}
+        RangeSelectingenemy = false;
+        selectingfriend = false;
+        unselectaround = false;
+        isAoE = false;
+        aoeradius = 0; MouseControler.SkillState = false;
+    }
 
 
     #region Skill1
     public void Skill1()
     {
         Debug.LogError("działam");
-      
+
         if (kochamizabelke < 2 && mouseControler.getHexUnderMouse() != mouseControler.getSelectedToster().Hex && !mouseControler.getSelectedToster().Team.HexesUnderTeam.Contains(mouseControler.getHexUnderMouse()) && mouseControler.getHexUnderMouse().Tosters.Count > 0 && mouseControler.getHexUnderMouse().Tosters.Count > 0)
         {
             isInProgress = true;
@@ -64,10 +64,10 @@ public class CastManager : MonoBehaviour
             Debug.Log("Nie ma tosta na tym polu");
             //yield return null;
         }
-     
+
 
     }
- 
+
 
     public void Skill1M()
     {
@@ -83,11 +83,12 @@ public class CastManager : MonoBehaviour
         Debug.LogError("działam");
         List<HexClass> hexarea = new List<HexClass>(mouseControler.getHexUnderMouse().hexMap.GetHexesWithinRadiusOf(mouseControler.getHexUnderMouse(), aoeradius));
         foreach (HexClass t in hexarea)
-        {if (t!=null)
-            if (t.Tosters.Count>0)
-            {
-                t.Tosters[0].DealMePURE(100);
-            }
+        {
+            if (t != null)
+                if (t.Tosters.Count > 0)
+                {
+                    t.Tosters[0].DealMePURE(100);
+                }
         }
 
         SetFalse();
@@ -130,5 +131,100 @@ public class CastManager : MonoBehaviour
         selectingfriend = true;
 
     }
-    #endregion 
+    #endregion
+
+
+    #region FRACTION STRUCUTRE
+    /*
+      
+    /////////*** FRACTION_NAME *** //////////// 
+
+        #region Fraction_Name
+
+            #region Unit_Name
+
+
+                    #region Unit_Name_Skill_1
+
+                    #endregion
+
+                    #region Unit_Name_Skill_2
+
+                    #endregion
+
+
+                    #region Unit_Name_Skill_3
+
+                    #endregion
+
+
+                    #region Unit_Name_Skill_4
+
+                    #endregion
+
+            #endregion
+
+        #endregion
+    */
+    #endregion
+
+    /////////*** Barbarians *** //////////// 
+    #region Barbarians
+
+    #region Szaman
+
+
+    #region Szaman_Skill_1
+
+    #endregion
+
+    #region Szaman_Skill_2
+
+    #endregion
+
+
+    #region Szaman_Skill_3
+
+    #endregion
+
+
+    #region Szaman_Skill_4
+
+    #endregion
+
+    #endregion
+
+    #region Rzutnik
+
+
+
+    // 10 % otrzymanych obrażeń przechodzi na koniec następnej tury
+    #region Rzutnik_Skill_1
+
+
+    #endregion
+
+    #region Rzutnik_Skill_2
+    // Zabiera 10 % swojego całkowitego HP, zadaje +8% dmg
+
+    #endregion
+
+
+    #region Rzutnik_Skill_3
+    // Rzut w dwóch przeciwników na raz(po 40% dmg)
+
+    #endregion
+
+
+    #region Rzutnik_Skill_4
+
+    #endregion
+
+    #endregion
+
+    #endregion
+
+
+
+
 }
