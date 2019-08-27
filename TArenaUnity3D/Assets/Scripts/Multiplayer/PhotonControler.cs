df54058d2d478a8417693d1546167f6dffbb8c06
Piotr Musielski <piotrmusielski@mail.com>
2019-08-27 20:27:46 +0200

    PUN 2

---
+++ TArenaUnity3D/Assets/Scripts/Multiplayer/PhotonControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using UnityEngine.UI;
+public class PhotonControler : Photon.PunBehaviour 
+{
+    public string GameVersion;
+    public Text ConnectState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FixedUpdate()
+    {
+        ConnectState.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+    public void connecttogheter()
+    {
+        
+        PhotonNetwork.ConnectUsingSettings(GameVersion);
+    }
+}
