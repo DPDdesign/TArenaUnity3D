1563c1df36f965c805e7e247936c48d22dcfa827
Piotr Musielski <piotrmusielski@mail.com>
2019-09-12 14:20:11 +0200

    Nowe UI
    Nowe UI

--- TArenaUnity3D/Assets/OverlayMainMenu.cs
+++ TArenaUnity3D/Assets/OverlayMainMenu.cs
@@ -7,6 +7,10 @@ using UnityEngine.UI;
 
 public class OverlayMainMenu : MonoBehaviour
 {
+    public List<GameObject> panels = new List<GameObject>();
+    public List<GameObject> buttons = new List<GameObject>();
+    public List<Color> buttonColorsOn = new List<Color>();
+    public List<Color> buttonColorsOff = new List<Color>();
     float LevelProgress;
     public Slider LevelSlider;
     public Text TCn, ATn;
@@ -30,32 +34,57 @@ public class OverlayMainMenu : MonoBehaviour
             PlayFabControler.PFC.GetInventory();
             PlayFabControler.PFC.GetStats();
         }
-        else SceneManager.LoadScene("LogIn");
+
+        else Debug.Log("OFFLINE");
+       // else SceneManager.LoadScene("LogIn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        ShowStats();
+       // ShowStats();
     }
 
     // Start is called before the first frame update
   
+    public void OnClickOverlayButton(int i)
+    {
+
+
+    }
 
     public void OnClickShowDetails()
     {
         DetailsPanel.SetActive(!DetailsPanel.activeSelf);
-      
-
-
     }
+
     public void OnClickShowShop()
     {
-        shop.SetActive(!shop.activeSelf);
-
-
-
+        ShowPanelOnly(1);
+        if (shop.activeSelf == true)  buttons[1].GetComponent<Image>().color = new Color(1,0.6f,0,1);
+        else buttons[1].GetComponent<Image>().color = new Color(1,0.9f,0,1);
     }
+
+
+public void ShowPanelOnly(int PanelNumber)
+{
+     panels[PanelNumber].SetActive(true);
+     buttons[PanelNumber].GetComponent<Image>().color = buttonColorsOn[PanelNumber];
+      buttons[PanelNumber].GetComponentInChildren<Text>().color = Color.white;
+        int i = 0;
+    foreach (var GameObject in panels)
+    {
+        if(i!=PanelNumber)
+        {
+         panels[i].SetActive(false);   
+          buttons[i].GetComponent<Image>().color = buttonColorsOff[i];
+          buttons[i].GetComponentInChildren<Text>().color = Color.black;
+           
+        }
+        i++;
+    }
+}
+
     float WinRatio;
     void ShowStats()
     {
