f36c9560be54cd3f3249b30e32894bbc154e83d1
Piotr Musielski <piotrmusielski@mail.com>
2019-11-23 13:36:09 +0100

    AI Reverse
    Przywrócono stare AI, ale dopisano mu nowe funkcje logiczne

--- TArenaUnity3D/Assets/Scripts/Lesisz/MostStupidAIEver.cs
+++ TArenaUnity3D/Assets/Scripts/Lesisz/MostStupidAIEver.cs
@@ -23,44 +23,12 @@ public class MostStupidAIEver : MonoBehaviour
 
     public void AskAIwhattodo()
     {
-        
+
         MC.getSelectedToster();
 
         //      List<HexClass> hexarea = new List<HexClass>(mouseControler.getHexUnderMouse().hexMap.GetHexesWithinRadiusOf(mouseControler.getHexUnderMouse(), aoeradius);
         List<HexClass> EnemyHexes = MC.GetEnemy();
         HexClass tempCel = null;
-<<<<<<< HEAD
-
-
-
-        #region firegolem
-        
-               // Debug.Log("toster o najmniejszym hp: " + TosterWithLeastHP(EnemyTosters).Name);
-                List<TosterHexUnit> PDamageList = ListOfDamageFromPlayer(EnemyTosters,AIToster);
-                List<TosterHexUnit> AIDamageList = ListOfDamageToPlayer(EnemyTosters,AIToster);
-                List<TosterHexUnit> PriorityList = ListOfTradeValues(EnemyTosters,AIToster);
-                //Debug.Log(AIToster.Name + "MYSLI: ");
-
-                foreach(TosterHexUnit unit in PDamageList)
-                {
-                   // Debug.Log(unit.Name + " zada mi " + AIToster.CalculateDamageBetweenTosters(unit,AIToster,0));
-                }
-
-                  foreach(TosterHexUnit unit in AIDamageList)
-                {
-                  //  Debug.Log(unit.Name + " zadam mu " + AIToster.CalculateDamageBetweenTosters(AIToster,unit,0));
-                }
-
-        
-        #endregion firegolem
-
-
-
-
-
-        #region Stupid attack
-=======
->>>>>>> parent of 6fcb09f... AI Upgrade
         foreach (HexClass h in EnemyHexes)
         {
 
@@ -80,76 +48,165 @@ public class MostStupidAIEver : MonoBehaviour
                 hexarea.Remove(h);
                 foreach (HexClass hex in hexarea)
                 {
-                    if (hex != null && hex.Tosters.Count==0)
-<<<<<<< HEAD
-                    { 
-                       
-                         Debug.Log("Toster o nazwie: " + h.Tosters[0].Name+ "Stoi na hex ( " + h.C + " , " + h.R + " )");
-
-                        if (AIToster.IsPathAvaible(hex))
-=======
+                    if (hex != null && hex.Tosters.Count == 0)
                     {
-                        
-                        //  tempCel = h.hexMap.GetHexAt(h.C, h.R - 1);
-                      
+
+                          //tempCel = h.hexMap.GetHexAt(h.C, h.R - 1);
+
                         if (MC.getSelectedToster().IsPathAvaible(hex))
->>>>>>> parent of 6fcb09f... AI Upgrade
                         {
-                           
-                            MC.StartCoroutine(MC.DoMoveAndAttackWithoutCheck(hex, h.Tosters[0]));
+                            MC.StartCoroutine(MC.DoMoveAndAttackWithoutCheck(hex, h.Tosters[0],MC.getSelectedToster()));
                             return;
                         }
                     }
                 }
-                
+
             }
         }
         int count = 999;
         int hNo = 0;
         int tempi = 0;
-        List<HexClass> hexpath = new List<HexClass>(); 
+        List<HexClass> hexpath = new List<HexClass>();
         List<HexClass> hexmaxpath = new List<HexClass>();
         foreach (HexClass h in EnemyHexes)
         {
-           
+
             if (h.Tosters[0].isDead == false)
             {
-                 hexpath = new List<HexClass>(AIToster.Pathing(h, true));
-                if (hexpath.Count<count)
+                hexpath = new List<HexClass>(MC.getSelectedToster().Pathing(h, true));
+                if (hexpath.Count < count)
                 {
                     count = hexpath.Count;
                     hNo = tempi;
                 }
             }
             tempi++;
-      }
+        }
 
-        hexpath = new List<HexClass>(AIToster.Pathing(EnemyHexes[hNo], true));
+        hexpath = new List<HexClass>(MC.getSelectedToster().Pathing(EnemyHexes[hNo], true));
         hexmaxpath = new List<HexClass>();
         for (int i = 0; i < MC.getSelectedToster().GetMS(); i++)
             hexmaxpath.Add(hexpath[i]);
-     
-        StartCoroutine(MC.DoMovesPath(hexmaxpath));
+
+        StartCoroutine(MC.DoMovesST(hexmaxpath[hexmaxpath.Count-1],MC.getSelectedToster()));
         return;
 
 
     }
-<<<<<<< HEAD
-
-    double MaxDamageToDeal(List<TosterHexUnit> tosters, TosterHexUnit ai)
+    #region ANALIZA
+    TosterHexUnit TosterWithLeastHP(List<TosterHexUnit> tosters)
     {
-        double i = ai.CalculateDamageBetweenTosters(ai,ListOfDamageToPlayer(tosters, ai)[0],0);
+        TosterHexUnit target = null;
+        foreach (TosterHexUnit t in tosters)
+        {
+            if (target == null)
+            {
+                target = t;
+            }
+            else if ((t.Amount - 1) * t.GetHP() + t.TempHP < (target.Amount - 1) * target.GetHP() + target.TempHP)
+            {
+                target = t;
+            }
+        }
+        return target;
+    }
+
+    /// Sortuje malejąco jednostki przeciwnika względem zadawanego przez nie damage jednostce.
+    List<TosterHexUnit> ListOfDamageFromPlayer(List<TosterHexUnit> tosters, TosterHexUnit ai)
+    {
+        List<TosterHexUnit> target = new List<TosterHexUnit>();
+
+        foreach (TosterHexUnit t in tosters)
+        {
+            // pierwszy toster
+            if (target.Count == 0)
+            {
+                target.Add(t);
+            }
+
+            // Jezeli zadaje wiecej - daj go na poczatek
+            else if (ai.CalculateDamageBetweenTosters(t, ai, 0) > ai.CalculateDamageBetweenTosters(target[0], ai, 0))
+            {
+                target.Insert(0, t);
+            }
+
+            // Jezeli zadaje mniej - sortuj
+            else
+            {
+                bool sorting = true;
+                int i = target.Count - 1;
+                while (sorting)
+                {
+                    if (ai.CalculateDamageBetweenTosters(t, ai, 0) <= ai.CalculateDamageBetweenTosters(target[i], ai, 0))
+                    {
+                        target.Insert(i + 1, t);
+                        sorting = false;
+                    }
+                    i--;
+                }
+            }
+        }
+        return target;
+    }
+
+    double MaxDamageToGet(List<TosterHexUnit> tosters, TosterHexUnit ai)
+    {
+        double i = ai.CalculateDamageBetweenTosters(ListOfDamageFromPlayer(tosters, ai)[0], ai, 0);
         return i;
     }
 
-/// Wylicza wartosc oszczedzonych obrazen
-     List<TosterHexUnit> ListOfTradeValues(List<TosterHexUnit> tosters, TosterHexUnit ai)
+    /// Sortuje malejąco jednostki przeciwnika względem otrzymane przez nie damage od jednostki.
+    List<TosterHexUnit> ListOfDamageToPlayer(List<TosterHexUnit> tosters, TosterHexUnit ai) //Tworzy liste To
+    {
+        List<TosterHexUnit> target = new List<TosterHexUnit>();
+
+        foreach (TosterHexUnit t in tosters)
+        {
+            // pierwszy toster
+            if (target.Count == 0)
+            {
+                target.Add(t);
+            }
+
+            // Jezeli otrzyma wiecej - daj go na poczatek
+            else if (ai.CalculateDamageBetweenTosters(ai, t, 0) > ai.CalculateDamageBetweenTosters(ai, target[0], 0))
+            {
+                target.Insert(0, t);
+            }
+
+            // Jezeli otrzyma mniej - sortuj
+            else
+            {
+                bool sorting = true;
+                int i = target.Count - 1;
+                while (sorting)
+                {
+                    if (ai.CalculateDamageBetweenTosters(ai, t, 0) <= ai.CalculateDamageBetweenTosters(ai, target[i], 0))
+                    {
+                        target.Insert(i + 1, t);
+                        sorting = false;
+                    }
+                    i--;
+                }
+            }
+        }
+        return target;
+    }
+
+    double MaxDamageToDeal(List<TosterHexUnit> tosters, TosterHexUnit ai)
+    {
+        double i = ai.CalculateDamageBetweenTosters(ai, ListOfDamageToPlayer(tosters, ai)[0], 0);
+        return i;
+    }
+
+    /// Wylicza wartosc oszczedzonych obrazen
+    List<TosterHexUnit> ListOfTradeValues(List<TosterHexUnit> tosters, TosterHexUnit ai)
     {
 
         Debug.Log("**********************************************************");
         Debug.Log(ai.Name + "  MOWI: OBLICZAM WARTOSCI TRADE OF");
-        List<TosterHexUnit> target = new List<TosterHexUnit>();   
-        
+        List<TosterHexUnit> target = new List<TosterHexUnit>();
+
         // 1. POLICZ ILE KAZDY TOSTER ZADA CI DMG
         // 2. POLICZ ILE DMG ZADASZ TEMU TOSTEROWI
         // 2.1 POLICZ ILE ZGINIE TOSTERÓW W WYNIKU ATAKU
@@ -157,62 +214,64 @@ public class MostStupidAIEver : MonoBehaviour
         // 4. OBLICZ RÓŻNICĘ MIĘDZY 4 a 1
 
         // 1.
-        List<TosterHexUnit> PDamageList = ListOfDamageFromPlayer(tosters,ai);
+        List<TosterHexUnit> PDamageList = ListOfDamageFromPlayer(tosters, ai);
 
         List<int> NewQuantieties = new List<int>();
-        int i=0;
-        double damagedifftemp=0;
+        int i = 0;
+        double damagedifftemp = 0;
         double damagediff;
 
         foreach (TosterHexUnit toster in PDamageList)
         {
 
-            double damage = ai.CalculateDamageBetweenTosters(toster,ai,0);
+            double damage = ai.CalculateDamageBetweenTosters(toster, ai, 0);
             Debug.Log(toster.Name + " zada mi " + damage);
 
 
             // ZJEBANE BO ZLE LICZY ILOSC
-            double cdamage = ai.CalculateDamageBetweenTosters(ai,toster,0);
+            double cdamage = ai.CalculateDamageBetweenTosters(ai, toster, 0);
             Debug.Log("Zadam " + cdamage + " " + toster.Name);
-            NewQuantieties.Add( toster.Amount - Mathf.FloorToInt( (float)cdamage/toster.GetHP() ) );
+            NewQuantieties.Add(toster.Amount - Mathf.FloorToInt((float)cdamage / toster.GetHP()));
 
 
-            double damage2 = ai.CalculateDamageBetweenTostersWithQ(toster,ai,0,NewQuantieties[i]);
+            double damage2 = ai.CalculateDamageBetweenTostersWithQ(toster, ai, 0, NewQuantieties[i]);
             i++;
-            Debug.Log("Teraz " + toster.Name + " Zada mi "+ damage2.ToString());
-            damagediff = damage-damage2;
+            Debug.Log("Teraz " + toster.Name + " Zada mi " + damage2.ToString());
+            damagediff = damage - damage2;
 
             Debug.Log("dzieki temu oszczedze " + damagediff);
 
-            if (target.Count == 0){
+            if (target.Count == 0)
+            {
                 target.Add(toster);
                 damagedifftemp = damagediff;
             }
 
             else if (damagediff > damagedifftemp)
             {
-              target.Insert(0,toster);
-              damagedifftemp = damagediff;
+                target.Insert(0, toster);
+                damagedifftemp = damagediff;
             }
 
             // ZJEBANE BO NIE SORTUJE :X
-            else 
+            else
             {
                 target.Add(toster);
             }
 
         }
-//Amount = Mathf.FloorToInt(newhp / GetHP());
+        //Amount = Mathf.FloorToInt(newhp / GetHP());
         Debug.Log("**********************************************************");
         Debug.Log(ai.Name + "  WYCIAGA WNIOSEK: Powinienem focusowac" + target[0].Name);
         return target;
     }
+    #endregion
 
 
 
 
 
-    
-=======
->>>>>>> parent of 6fcb09f... AI Upgrade
 }
+
+
+
